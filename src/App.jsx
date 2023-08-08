e3f2b4bf8125b6234daf5ef6741ae3c4cadaa4bc
Misty Becker <mistyb01@uw.edu>
2023-08-07 20:57:32 -0700

    testing dayJS

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,47 @@
 import { useState, useEffect } from 'react'
+import dayjs from 'dayjs'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [remainingTime, setRemainingTime] = useState({
+    minutes: '25',
+    seconds: '00'
+  })
   const [timerActive, setTimerActive] = useState(false)
+  const [timerStartTime, setTimerStartTime] = useState(null)
 
   useEffect(() => {
     let id;
     if (timerActive) {
+      console.log('start', timerStartTime)
       id = setInterval(() => {
-        setCount(c => c + 1)
+        let currentTime = dayjs();
+        console.log('start', timerStartTime.format('HH:mm:ss'), 'current', currentTime.format('HH:mm:ss'))
       }, 1000)
+      return () => clearInterval(id)
     }
-
-    return(() => {
-      clearInterval(id)
-    })
   }, [timerActive])
+
+  function handleTimerStart() {
+    setTimerStartTime(dayjs()); // set to current time, from dayJS
+    setTimerActive(!timerActive)
+  }
 
   return (
     <>
       <h1>pomos</h1>
-      <h2>{count}</h2>
-      <button onClick={() => setTimerActive(!timerActive)}>
+      <Timer>{remainingTime.minutes} : {remainingTime.seconds}</Timer>
+
+
+      <button onClick={handleTimerStart}>
         {timerActive ? 'pause' : 'start'} timer</button>
+
     </>
+  )
+}
+
+function Timer({children}) {
+  return(
+    <h2>{children}</h2>
   )
 }
 
