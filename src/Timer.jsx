c389063f7dea34a667fe0e56673a17c78da76829
Misty <60805050+mistyb01@users.noreply.github.com>
2023-08-08 09:20:05 -0700

    Basic timer functionality (#1)
    * basic useEffect for counter
    
    * testing dayJS
    
    * calculate diff
    
    * calculates remaining value, in seconds
    
    * calculates remaining mins and secs
    
    * UI updates
    
    * timer continues from paused time
    
    * timer stops when it reaches 0
    
    * created separate Timer component
    
    * reset function works

---
+++ src/Timer.jsx
@@ -0,0 +1,79 @@
+import { useState, useEffect } from "react";
+import dayjs from "dayjs";
+
+function Timer({ startMin, startSecs }) {
+  const [timerActive, setTimerActive] = useState(false);
+  const [timerStartTime, setTimerStartTime] = useState(null);
+
+  const initialSettings = {
+    minutes: startMin,
+    seconds: startSecs,
+  };
+
+  const [remainingTime, setRemainingTime] = useState(initialSettings);
+  const [initialTime, setInitialTime] = useState(initialSettings);
+
+  useEffect(() => {
+    let id;
+    let remainingTimeMins;
+    let remainingTimeSecs;
+
+    if (timerActive) {
+      id = setInterval(() => {
+        let currentTime = dayjs();
+        let diffInSecs = currentTime.diff(timerStartTime, "seconds");
+
+        const initialInSecs = initialTime.minutes * 60 + initialTime.seconds;
+        const remainingInSecs = initialInSecs - diffInSecs;
+
+        remainingTimeSecs = remainingInSecs % 60;
+        remainingTimeMins = Math.floor(remainingInSecs / 60);
+
+        setRemainingTime({
+          minutes: remainingTimeMins,
+          seconds: remainingTimeSecs,
+        });
+
+        if (remainingTimeMins === 0 && remainingTimeSecs === 0) {
+          setTimerActive(!timerActive);
+        }
+      }, 1000);
+
+      return () => {
+        clearInterval(id);
+        setInitialTime({
+          minutes: remainingTimeMins,
+          seconds: remainingTimeSecs,
+        });
+      };
+    }
+  }, [timerActive, timerStartTime]);
+
+  function handleTimerStart() {
+    setTimerStartTime(dayjs()); // set to current time, from dayJS
+    setTimerActive(!timerActive);
+  }
+
+  function handleTimerReset() {
+    setTimerStartTime(dayjs()); // set to current time, from dayJS
+    setInitialTime(initialSettings);
+    setRemainingTime(initialSettings);
+  }
+
+  return (
+    <>
+      <div className="timer-container">
+        {remainingTime.minutes}:
+        {remainingTime.seconds.toString().padStart(2, "0")}
+      </div>
+
+      <button onClick={handleTimerStart}>
+        {timerActive ? "pause" : "start"} timer
+      </button>
+
+      <button onClick={handleTimerReset}>reset</button>
+    </>
+  );
+}
+
+export default Timer;
