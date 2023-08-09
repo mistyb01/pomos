4905b5da8ae6bc91bd41f63ba0bbb8940a5b41a2
Misty <60805050+mistyb01@users.noreply.github.com>
2023-08-09 13:23:47 -0700

    Cycle functionality (#2)
    * pass in cycle prop to Timer
    
    * able to go to next session in cycle
    
    * fixed bug (removed remainingTime as a dependency)
    
    * congrats message on cycle complete
    
    * reset cycle function

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,16 +1,15 @@
 import { useState, useEffect } from "react";
 import dayjs from "dayjs";
 
-function Timer({ startMin, startSecs }) {
+function Timer({ cycle }) {
   const [timerActive, setTimerActive] = useState(false);
   const [timerStartTime, setTimerStartTime] = useState(null);
+  const [cycleIndex, setCycleIndex] = useState(0);
 
-  const initialSettings = {
-    minutes: startMin,
-    seconds: startSecs,
-  };
+  const initialTime = cycle[cycleIndex];
+  const [remainingTime, setRemainingTime] = useState(null);
 
-  const [remainingTime, setRemainingTime] = useState(initialSettings);
+  const [isCycleComplete, setIsCycleComplete] = useState(false);
 
   useEffect(() => {
     let id;
@@ -22,9 +21,14 @@ function Timer({ startMin, startSecs }) {
         let currentTime = dayjs();
         let diffInSecs = currentTime.diff(timerStartTime, "seconds");
 
-        const initialInSecs =
-          remainingTime.minutes * 60 + remainingTime.seconds;
-        const remainingInSecs = initialInSecs - diffInSecs;
+        let initialInSecs;
+        if (!remainingTime) {
+          initialInSecs = initialTime.minutes * 60 + initialTime.seconds;
+        } else {
+          initialInSecs = remainingTime.minutes * 60 + remainingTime.seconds;
+        }
+
+        let remainingInSecs = initialInSecs - diffInSecs;
 
         remainingTimeSecs = remainingInSecs % 60;
         remainingTimeMins = Math.floor(remainingInSecs / 60);
@@ -35,7 +39,13 @@ function Timer({ startMin, startSecs }) {
         });
 
         if (remainingTimeMins === 0 && remainingTimeSecs === 0) {
-          setTimerActive(!timerActive);
+          setTimerActive(false);
+          if (cycleIndex + 1 < cycle.length) {
+            handleTimerNext();
+          } else {
+            // reached end of cycle
+            setIsCycleComplete(true);
+          }
         }
       }, 1000);
 
@@ -43,7 +53,7 @@ function Timer({ startMin, startSecs }) {
         clearInterval(id);
       };
     }
-  }, [timerActive, timerStartTime]);
+  }, [timerActive, timerStartTime, cycleIndex]);
 
   function handleTimerStart() {
     setTimerStartTime(dayjs()); // set to current time, from dayJS
@@ -52,21 +62,96 @@ function Timer({ startMin, startSecs }) {
 
   function handleTimerReset() {
     setTimerStartTime(dayjs()); // set to current time, from dayJS
-    setRemainingTime(initialSettings);
+    setRemainingTime(cycle[cycleIndex]);
+  }
+
+  function handleTimerNext() {
+    setTimerStartTime(dayjs()); // set to current time, from dayJS
+    setCycleIndex((c) => c + 1);
+    setRemainingTime(null);
+  }
+
+  function handleCycleReset() {
+    setCycleIndex(0);
+    setRemainingTime(null);
   }
 
   return (
     <>
       <div className="timer-container">
-        {remainingTime.minutes}:
-        {remainingTime.seconds.toString().padStart(2, "0")}
+        {remainingTime
+          ? `${remainingTime.minutes}:${remainingTime.seconds
+              .toString()
+              .padStart(2, "0")}`
+          : `${initialTime.minutes}:${initialTime.seconds
+              .toString()
+              .padStart(2, "0")}`}
       </div>
 
+      <h3>{initialTime.mode} mode</h3>
+      <h4>
+        session {cycleIndex + 1} / {cycle.length}
+      </h4>
       <button onClick={handleTimerStart}>
         {timerActive ? "pause" : "start"} timer
       </button>
 
-      <button onClick={handleTimerReset}>reset</button>
+      <button onClick={handleTimerReset}>reset timer</button>
+
+      <br />
+      <br />
+
+      {cycleIndex + 1 < cycle.length && (
+        <button onClick={handleTimerNext}>skip this session</button>
+      )}
+
+      <br />
+      <br />
+
+      {cycleIndex !== 0 && (
+        <button onClick={handleCycleReset}>start over cycle</button>
+      )}
+
+      {isCycleComplete && (
+        <>
+          <h3>you finished!</h3>
+          <p>
+            total work time:
+            {cycle
+              .filter((session) => session.mode === "work")
+              .reduce(
+                (accumulator, currentValue) =>
+                  accumulator +
+                  currentValue.minutes +
+                  currentValue.seconds / 60,
+                0
+              )}
+          </p>
+          <p>
+            total break time:
+            {cycle
+              .filter(
+                (session) =>
+                  session.mode === "break" || session.mode === "long break"
+              )
+              .reduce(
+                (accumulator, currentValue) =>
+                  accumulator +
+                  currentValue.minutes +
+                  currentValue.seconds / 60,
+                0
+              )}
+          </p>
+          <p>
+            total time overall:
+            {cycle.reduce(
+              (accumulator, currentValue) =>
+                accumulator + currentValue.minutes + currentValue.seconds / 60,
+              0
+            )}
+          </p>
+        </>
+      )}
     </>
   );
 }
