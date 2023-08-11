eff1caff30397b1b94c0c9248c56c288eba963a7
Misty <60805050+mistyb01@users.noreply.github.com>
2023-08-11 15:52:56 -0700

    browser notifs with Notification API (#6)

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,20 @@ import { useState, useEffect } from "react";
 import Timer from "./Timer";
 
 function App() {
+  useEffect(() => {
+    if (!("Notification" in window)) {
+      console.log("Browser does not support desktop notification");
+    } else {
+      Notification.requestPermission();
+    }
+  }, []);
+
   const cycle = [
     { mode: "work", minutes: 20, seconds: 0 },
     { mode: "break", minutes: 5, seconds: 0 },
     { mode: "work", minutes: 20, seconds: 0 },
     { mode: "break", minutes: 5, seconds: 0 },
-    { mode: "work", minutes: 20, seconds: 5 },
+    { mode: "work", minutes: 20, seconds: 0 },
   ];
 
   return (
