47f35c06032f53534b622632b6f15ec5d0c1ff95
Misty <60805050+mistyb01@users.noreply.github.com>
2023-08-15 12:45:30 -0700

    Cycle editor functionality  (#8)
    * settings icon
    
    * slide-out menu
    
    * createCycle function
    
    * form updates cycleData state
    
    * editor css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { useState, useEffect } from "react";
 import Timer from "./Timer";
+import SettingsIcon from "./components/icons/SettingsIcon";
+import CycleEditor from "./components/CycleEditor";
 
 function App() {
+  const [showSettings, setShowSettings] = useState(false);
+
   useEffect(() => {
     if (!("Notification" in window)) {
       console.log("Browser does not support desktop notification");
@@ -10,16 +14,60 @@ function App() {
     }
   }, []);
 
-  const cycle = [
-    { mode: "work", minutes: 20, seconds: 0 },
-    { mode: "break", minutes: 5, seconds: 0 },
-    { mode: "work", minutes: 20, seconds: 0 },
-    { mode: "break", minutes: 5, seconds: 0 },
-    { mode: "work", minutes: 20, seconds: 0 },
-  ];
+  const [cycleData, setCycleData] = useState({
+    workMins: 25,
+    workSessions: 5,
+    breakMins: 5,
+    longBreak: false,
+    longBreakMins: 10,
+  });
+
+  function createCycle() {
+    let cycleArr = [];
+    for (let i = 0; i < cycleData.workSessions; i++) {
+      cycleArr.push({ mode: "work", minutes: cycleData.workMins, seconds: 0 });
+      if (i + 1 < cycleData.workSessions) {
+        cycleArr.push({
+          mode: "break",
+          minutes: cycleData.breakMins,
+          seconds: 0,
+        });
+      }
+    }
+    if (cycleData.longBreak) {
+      cycleArr.push({
+        mode: "longBreak",
+        minutes: cycleData.longBreakMins,
+        seconds: 0,
+      });
+    }
+    return cycleArr;
+  }
+
+  const cycle = createCycle();
 
   return (
     <>
+      <div
+        className="settings-icon-container"
+        onClick={() => setShowSettings(!showSettings)}
+      >
+        <SettingsIcon />
+      </div>
+
+      <section
+        className={
+          showSettings
+            ? "settings-container settings-container-active"
+            : "settings-container"
+        }
+      >
+        <CycleEditor
+          cycleData={cycleData}
+          updateCycle={(newData) => setCycleData({ ...newData })}
+        />
+      </section>
+
       <main>
         <Timer cycle={cycle} />
       </main>
---
+++ src/components/CycleEditor.jsx
@@ -0,0 +1,87 @@
+import { useState } from "react";
+
+export default function CycleEditor({ cycleData, updateCycle }) {
+  const [formData, setFormData] = useState(cycleData);
+
+  function handleInputChange(e) {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  function handleLongBreakChange(e) {
+    setFormData({
+      ...formData,
+      longBreak: e.target.checked,
+    });
+  }
+
+  return (
+    <section className="cycle-editor">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          updateCycle(formData);
+        }}
+      >
+        <h2>Edit Cycle</h2>
+
+        <div className="form-row">
+          <input
+            type="number"
+            name="workMins"
+            value={formData.workMins}
+            onChange={handleInputChange}
+          ></input>
+          <label htmlFor="workMins">work mins</label>
+        </div>
+
+        <div className="form-row">
+          <input
+            type="number"
+            value={formData.workSessions}
+            name="workSessions"
+            onChange={handleInputChange}
+          ></input>
+          <label htmlFor="workSessions">work sessions</label>
+        </div>
+
+        <div className="form-row">
+          <input
+            type="number"
+            name="breakMins"
+            value={formData.breakMins}
+            onChange={handleInputChange}
+          ></input>
+          <label htmlFor="breakMins">break mins</label>
+        </div>
+
+        <div className="form-row">
+          <input
+            type="checkbox"
+            name="longBreak"
+            checked={formData.longBreak}
+            onChange={handleLongBreakChange}
+          ></input>
+          <label htmlFor="longBreak">end with long break</label>
+        </div>
+
+        {formData.longBreak && (
+          <div className="form-row">
+            <input
+              type="number"
+              name="longBreakMins"
+              value={formData.longBreakMins}
+              onChange={handleInputChange}
+            ></input>
+            <label htmlFor="longBreak">end with long break</label>
+          </div>
+        )}
+        <button className="form-btn" type="submit">
+          save
+        </button>
+      </form>
+    </section>
+  );
+}
