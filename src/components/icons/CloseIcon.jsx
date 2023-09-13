a429db8ffdb53397b4b900fee70a69612c0478ba
Misty <60805050+mistyb01@users.noreply.github.com>
2023-09-13 10:45:22 -0700

    fix: improved mobile UI (#14)
    * settings menu scrollable, button aspect ratio
    
    * icons resize
    
    * timer buttons shrink
    
    * flex direction of option btns
    
    * color and settings scrollbar fix

--- src/components/icons/CloseIcon.jsx
+++ src/components/icons/CloseIcon.jsx
@@ -2,9 +2,8 @@ export default function CloseIcon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      height="48"
       viewBox="0 -960 960 960"
-      width="48"
+      className="ui-icon"
     >
       <path
         className="fill-accent"
--- src/components/icons/LightModeIcon.jsx
+++ src/components/icons/LightModeIcon.jsx
@@ -2,9 +2,8 @@ export default function LightModeIcon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      height="40"
       viewBox="0 -960 960 960"
-      width="40"
+      className="ui-icon"
     >
       <path
         className="fill-accent"
