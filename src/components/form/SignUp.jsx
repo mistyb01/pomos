e102e4dfdb3302fcbd175244aa2cb05573434991
Misty Becker <mistyb01@uw.edu>
2023-09-14 17:37:46 -0700

    sign up function creates user in supabase

--- src/components/form/SignUp.jsx
+++ src/components/form/SignUp.jsx
@@ -1,15 +1,48 @@
+import { useState } from "react";
+import supabase from "../../config/supabaseConfig";
+
 const SignUp = () => {
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+
+  const [error, setError] = useState(null);
+
+  const completeSignup = async (e) => {
+    e.preventDefault();
+    const { data, error } = await supabase.auth.signUp({
+      email: userEmail,
+      password: userPassword,
+    });
+    if (data) {
+      console.log(data);
+      setError(null);
+    }
+    if (error) {
+      setError("there was an error.");
+      console.log(error);
+    }
+  };
+
   return (
     <>
-      <form className="sign-in-form">
+      {error && <p>{error}</p>}
+      <form onSubmit={completeSignup} className="sign-in-form">
         <div>
           <label htmlFor="email">Email</label>
-          <input type="text" name="email" />
+          <input
+            type="text"
+            name="email"
+            onChange={(e) => setUserEmail(e.target.value)}
+          />
         </div>
 
         <div>
           <label htmlFor="password">Password</label>
-          <input type="text" name="password" />
+          <input
+            type="text"
+            name="password"
+            onChange={(e) => setUserPassword(e.target.value)}
+          />
         </div>
 
         <button type="submit" className="text-main">
