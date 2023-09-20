6739ea081db8ff9198eff59d82bc0a4a5c3de24b
Misty Becker <mistyb01@uw.edu>
2023-09-19 19:34:29 -0700

    indicator added to stats

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,15 +1,18 @@
-import { Bars } from "react-loader-spinner";
+import { Oval } from "react-loader-spinner";
 
 export default function Loading() {
   return (
-    <Bars
-      height="80"
-      width="80"
+    <Oval
+      height={80}
+      width={80}
       color="#4fa94d"
-      ariaLabel="bars-loading"
       wrapperStyle={{}}
       wrapperClass=""
       visible={true}
+      ariaLabel="oval-loading"
+      secondaryColor="#4fa94d"
+      strokeWidth={2}
+      strokeWidthSecondary={2}
     />
   );
 }
